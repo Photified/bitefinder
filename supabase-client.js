dcc2e0adada19c82677ed90bdc08c16afbfa7412
Photified <justinmainphoto@gmail.com>
2025-09-16 18:08:15 -0400

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,8 @@
 // FILE: supabase-client.js
 // This is the FIXED version
 
-const SUPABASE_URL = 'https://akaydadhkvpvyajnsvki.supabase.co';      // Paste your Project URL here
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrYXlkYWRoa3Zwdnlham5zdmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NTUzMDgsImV4cCI6MjA3MzQzMTMwOH0.sfrQ1ToVf2UnCir-vx6CTC8NSYjCy_htTKpPX09Ivio'; // Paste your anon (public) key here
+const SUPABASE_URL = 'https://lnsykgrjtgugnxufvruj.supabase.co';      // Paste your Project URL here
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxuc3lrZ3JqdGd1Z254dWZ2cnVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNTk2MjMsImV4cCI6MjA3MzYzNTYyM30.vwW4ziHiq-Wsx0eit78WHafQJ7vsfJ-4KY4bhbwniWw'; // Paste your anon (public) key here
 
 // Get the createClient function from the global 'supabase' object (loaded by the SDK script)
 const { createClient } = supabase;
